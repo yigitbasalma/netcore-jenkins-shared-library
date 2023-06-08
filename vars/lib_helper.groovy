fd4d2d93c5b62b1fb8e2a45ea99f89561f7791d9
Yiğit Can Başalma <yigit.basalma@local>
2023-06-08 15:04:41 +0300

    Fix

--- vars/lib_helper.groovy
+++ vars/lib_helper.groovy
@@ -43,10 +43,8 @@ def configureBranchDeployment(Map config, String sshKeyFile) {
 }
 
 def triggerJob(Map config) {
-    if ( ! config.sequential_deployment_mapping.containsKey(config.job_name) ) {
-        return true
+    if ( config.sequential_deployment_mapping.containsKey(config.job_name) ) {
+        next_job_name = config.sequential_deployment_mapping[config.job_name]
+        build job: "${config.job_base}/${next_job_name}", propagate: false, wait: false, parameters: [string(name: 'IMAGE', value: config.b_config.imageTag)]
     }
-
-    next_job_name = config.sequential_deployment_mapping[config.job_name]
-    build job: "${config.job_base}/${next_job_name}", propagate: false, wait: false, parameters: [string(name: 'IMAGE', value: config.b_config.imageTag)]
 }

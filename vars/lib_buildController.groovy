af4a0f8aea40679bc56ec8e88e2eaec58a1b044d
Yiğit Can Başalma <yigit.basalma@local>
2023-07-11 16:37:59 +0300

    Fix

--- vars/lib_buildController.groovy
+++ vars/lib_buildController.groovy
@@ -2,7 +2,7 @@ def call(Map config) {
     try {
         sh "${config.b_config.project.builderVersion} restore --no-cache ${config.b_config.project.solutionFilePath}"
     } catch (Exception e) {
-        sh "${config.b_config.project.builderVersion} restore --no-cache ${config.b_config.project.solutionFilePath}/${config.b_config.project[0].path}"
+        sh "${config.b_config.project.builderVersion} restore --no-cache ${config.b_config.project.solutionFilePath}/${config.b_config.projects[0].path}"
     }
 
     config.b_config.projects.each { it ->

5dd5098081edba3981c0197e607aed629746553b
Yiğit Can Başalma <yigit.basalma@local>
2023-09-27 13:50:52 +0300

    Fix

--- vars/lib_deployController.groovy
+++ vars/lib_deployController.groovy
@@ -52,7 +52,7 @@ def nativeK8s(Map config, String image, Map r_config, String containerRepository
     namespaceSelector = r_config.namespaceSelector
 
     if ( params.containsKey("TARGETS") && params.TARGETS != "" ) {
-        _targets = params.TARGETS.replace(',', '|')
+        _targets = params.TARGETS.trim().replace(",", "|")
         namespaceSelector = "(${_targets})-namespace"
     }
 

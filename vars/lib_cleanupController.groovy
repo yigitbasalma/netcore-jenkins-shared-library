f1b29f7fab5985f4e679b5b3ba024d36ecdd2fc7
Yiğit Can Başalma <yigit.basalma@local>
2023-10-11 11:52:25 +0300

    Fix

--- vars/lib_cleanupController.groovy
+++ vars/lib_cleanupController.groovy
@@ -3,4 +3,10 @@ def call(Map config) {
     rm -rf ${WORKSPACE}/* && \
     docker rmi \$(docker images -f "dangling=true" -q) 2> /dev/null || true
     """
+
+    if ( config.docker_volume_prune ) {
+        sh """
+        docker system prune --volumes -f
+        """
+    }
 }

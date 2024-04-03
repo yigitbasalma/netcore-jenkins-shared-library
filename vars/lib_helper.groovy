2290a177058d6748bd4cb5cc65cc985bd02f19ae
Yiğit Can Başalma <yigit.basalma@local>
2024-04-03 16:36:32 +0300

    Fix

--- vars/lib_helper.groovy
+++ vars/lib_helper.groovy
@@ -61,8 +61,10 @@ def configureBranchDeployment(Map config, String sshKeyFile) {
 def triggerJob(Map config) {
     try {
         if ( config.sequential_deployment_mapping.containsKey(config.job_name) && config.permit_trigger_branch.contains(config.target_branch) ) {
-            next_job_name = config.sequential_deployment_mapping[config.job_name]
-            build job: "${config.job_base}/${next_job_name}", propagate: false, wait: false, parameters: [string(name: 'IMAGE', value: config.b_config.imageTag), string(name: 'BRANCH', value: config.target_branch)]
+            config.sequential_deployment_mapping[config.job_name].split(",") {
+                next_job_name = it
+                build job: "${config.job_base}/${next_job_name}", propagate: false, wait: false, parameters: [string(name: 'IMAGE', value: config.b_config.imageTag), string(name: 'BRANCH', value: config.target_branch)]
+            }
         }
     } catch (Exception e) {
         echo "No job found for trigger."

c50ed81d6448cb882387b59c3775a3d5da661b83
Yiğit Can Başalma <yigit.basalma@local>
2024-04-21 13:02:34 +0300

    Fix

--- vars/lib_deployController.groovy
+++ vars/lib_deployController.groovy
@@ -43,7 +43,12 @@ def argocd(Map config, String image, Map r_config, String containerRepository) {
             }
 
             sh """#!/bin/bash
-            argocd app get ${appName}
+            argocd app get ${appName} \
+                --force \
+                --insecure \
+                --grpc-web \
+                --server ${config.b_config.argocd[config.environment].url} \
+                --auth-token $TOKEN || if grep "Running";then true; fi
             """
 
             sh """#!/bin/bash

63543ed60e2bb0c1bd52bfc6ba4d52de7335e290
Yiğit Can Başalma <yigit.basalma@local>
2025-08-12 21:58:56 +0300

    Fix

--- vars/lib_helper.groovy
+++ vars/lib_helper.groovy
@@ -1,4 +1,15 @@
 def configureInit(Map config) {
+    // If environment is production, prevent early deployment
+    if ( config.environment == "production" ) {
+        def now = new Date()
+        def currentHour = now.format('HH', TimeZone.getTimeZone('Europe/Istanbul')) as int
+        def currentMinute = now.format('mm', TimeZone.getTimeZone('Europe/Istanbul')) as int
+
+        if (currentHour < 23 || (currentHour == 23 && currentMinute < 30)) {
+            error("Build aborted: Current time is before 23:30")
+        }
+    }
+
     // Define constraints
     config.script_base = "/scripts"
     config.scope = "global"

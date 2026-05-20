1e6f6122847710cc421eb2ebcae7e6298501c085
Yiğit Can Başalma <yigit.basalma@local>
2026-05-20 11:56:40 +0300

    Fix

--- vars/lib_versionController.groovy
+++ vars/lib_versionController.groovy
@@ -2,10 +2,9 @@ def call(Map config) {
     // Configure project version if not configured
     if ( ! config.project_version ) {
         def full_version
-
-        if ( params.BRANCH ==~ /[pP]roduction/  ) {
+        if ( params.BRANCH ==~ /[pP]roduction/ ) {
             full_version = sh(
-                script: "python3 -c 'import sys,yaml,os; from datetime import datetime; b=int(os.environ['BUILD_NUMBER']); t=datetime.now(); major=yaml.load(open('config.yaml'),Loader=yaml.CLoader)['project']['version']; print(f\"{major}.{b//100}.{b%100}.{t.year-2000}{t.strftime('%j')}\")",
+                script: """python3 -c 'import sys,yaml,os; from datetime import datetime; b=int(os.environ["BUILD_NUMBER"]); t=datetime.now(); major=yaml.load(open("${config.config_file}"),Loader=yaml.CLoader)["project"]["version"]; print(f"{major}.{b//100}.{b%100}.{t.year-2000}{t.strftime(\"%j\")}")'""",
                 returnStdout: true
             ).trim()
         } else {
